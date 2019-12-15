253c9439369db50c144158053cf0a984566ae63d
NicholasTancredi <nicholastancredi@gmail.com>
2019-12-14 20:41:21 -0400

    first commit

---
+++ setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup, find_packages
+
+setup(name='ShutdownHandler',
+      version=open('VERSION').read(),
+      url='http://github.com/NicholasTancredi/ShutdownHandler',
+      packages=find_packages())
