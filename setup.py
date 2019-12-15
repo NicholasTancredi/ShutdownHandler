212774fedd531e41e7ebf2524dc82ec701157d15
NicholasTancredi <nicholastancredi@gmail.com>
2019-12-14 21:45:37 -0400

    Published Version:

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
 setup(name='ShutdownHandler',
-      version='1.0.10',
+      version='1.0.11',
       url='http://github.com/NicholasTancredi/ShutdownHandler',
       packages=find_packages())
